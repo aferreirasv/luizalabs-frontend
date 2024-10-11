e9e5edfaf3ab3ee40b5fa523f766d16260795312
Alan Ferreira <aferreirasv@gmail.com>
2024-10-11 11:22:33 -0300

    Adiciona contagem de páginas e validação para desabilitar os botões quando não houver mais páginas disponíveis

--- src/App.js
+++ src/App.js
@@ -1,59 +1,48 @@
 import './App.css';
 import React from 'react'
 import Order from './components/Order/Order'
+import axios from 'axios'
 
 function App() {
-  const orders = [{
-    id: "213719239",
-    customer: "Alan Ferreira",
-    status: "PENDENTE",
-    date:"2024-10-09T17:03:06Z" ,
-    cart: [
-      {
-        name: "ventilador 5 velocidades",
-        price: 190.90,
-        amount: 2
-      }
-    ],
-    shipping: 20.20,
-    subtotal:381.80,
-    total: 402.00
-  },
-  {
-    id: "187875454",
-    customer: "Alan Ferreira",
-    status: "PENDENTE",
-    date:"2024-10-09T17:03:06Z" ,
-    cart: [
-      {
-        name: "Aspirador de Pó Vertical e Portátil",
-        price: 190.00,
-        amount: 1
-      }
-    ],
-    shipping: 20.00,
-    subtotal:190.00,
-    total: 210.00
-  },
-  {
-    id: "2134634739",
-    customer: "Renan Ferreira",
-    status: "PENDENTE",
-    date:"2024-10-09T17:03:06Z" ,
-    cart: [
-      {
-        name: "ventilador 5 velocidades",
-        price: 190.90,
-        amount: 1
-      }
-    ],
-    shipping: 20.20,
-    subtotal:190.90,
-    total: 211.10
-  }]
-  // const [currentPage, setPage] = React.useState(1)
+  const baseURL = process.env.REACT_APP_BASEURL
+  const api = axios.create({
+    baseURL: baseURL
+  })
+  const ordersAmount = 3
+  const [totalPages, setTotalPages] = React.useState(0)
+  const [currentPage, setPage] = React.useState(1)
+  const [orders, setOrders] = React.useState([])
+
+  React.useEffect( () => {
+    getOrders(currentPage,ordersAmount)
+  }
+      ,[currentPage])
+
+  const nextPage = () => {
+    setPage(currentPage + 1)
+  }
+
+  const prevPage = () => {
+    setPage(currentPage - 1)
+  }
   
-  
+
+  async function getOrders(page, amount) {
+    try {
+
+        api.get(
+            `${baseURL}/orders?page=${page}&amount=${amount}`
+        ).then(res => {
+          setOrders(res.data.orders)
+          setTotalPages(Math.ceil(res.data.count/amount))
+        })
+        
+    }
+     catch( e ) {
+        console.error(e)
+     }
+}
+
   return (
     <>
     <header className='Header'>
@@ -66,10 +55,11 @@ function App() {
         })
       }
 
-    <div className='pageNavigator'>
-      <input className='pageButton' type='button' value="&lt;" />  
-      <input className='pageButton' type='button' value=" &gt;" />
-    </div>
+      <div className='pageNavigator'>
+        <input className='pageButton' type='button' value="&lt;" onClick={prevPage} disabled={!(currentPage > 1)}/>  
+        <p className='pageLabel'>{currentPage}</p>
+        <input className='pageButton' type='button' value="&gt;" onClick={nextPage} disabled={currentPage >= totalPages}/>
+      </div>
     </div>
     </>
   );
